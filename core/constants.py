cd7385e4baa0e253f571b7c8a270aeeb39dbccce
ahmetvaleevmaxim-arch <ahmetvaleevmaxim@gmail.com>
2026-07-06 14:48:55 +0300

    Create constants.py

---
+++ core/constants.py
@@ -0,0 +1,29 @@
+CITIES = [
+    "Miri",
+    "Bintulu",
+    "Labuan",
+    "Sibu",
+    "Seremban",
+]
+
+COMPETITORS = [
+    "Grab",
+    "inDrive",
+    "Bolt",
+    "AirAsia Ride",
+    "MyRide",
+]
+
+EVENT_TYPES = {
+    "country_news": "Новости страны",
+    "city_news": "Новости города",
+    "competitor": "Конкуренты",
+    "maxim": "Maxim Monitor",
+    "weather": "Погода",
+    "holiday": "Праздники",
+    "local_event": "Мероприятия",
+    "government": "Государство",
+    "transport": "Транспорт",
+    "opportunity": "Возможности",
+    "risk": "Риски",
+}
